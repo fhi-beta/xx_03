b61900be983709cfc795c58edbeceb2a36cac98f
Richard White <w@rwhite.no>
2020-10-06 13:06:29 +0200

    Small fix

--- Run_task1.R
+++ Run_task1.R
@@ -3,7 +3,7 @@ library(data.table)
 # You will need to change your working directory
 setwd("C:/Users/riwh/OneDrive - Folkehelseinstituttet/git/xx_03")
 
-fileSources = file.path("code", list.files("code_task1", pattern = "*.[rR]$"))
+fileSources = file.path("code_task1", list.files("code_task1", pattern = "*.[rR]$"))
 sapply(fileSources, source, .GlobalEnv)
 
 # code goes here
